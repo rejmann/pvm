ca639a3d582b518e83bd0a1072735c154e2485ca
Rejman <rejmans00@gmail.com>
2026-04-24 08:49:31 -0300

    fix: update both path "~/.pdv/bin/php" & "~/.local/bin/php"

--- internal/symlink/set.go
+++ internal/symlink/set.go
@@ -16,10 +16,33 @@ func SetCurrent(base, version, binaryPath string) error {
 		return fmt.Errorf("update-alternatives --set php %s: %w", binaryPath, err)
 	}
 
+	if err := updateSymlink(filepath.Join(base, "bin", "php"), binaryPath); err != nil {
+		return fmt.Errorf("update pvm bin symlink: %w", err)
+	}
+
+	localBin := filepath.Join(filepath.Dir(base), ".local", "bin", "php")
+	if fi, err := os.Lstat(localBin); err == nil && fi.Mode()&os.ModeSymlink != 0 {
+		if err := updateSymlink(localBin, binaryPath); err != nil {
+			return fmt.Errorf("update ~/.local/bin/php symlink: %w", err)
+		}
+	}
+
 	if err := os.WriteFile(filepath.Join(base, "current-version"), []byte(version), 0644); err != nil {
 		return fmt.Errorf("write current-version: %w", err)
 	}
 	return nil
+}
+
+func updateSymlink(link, target string) error {
+	if err := os.MkdirAll(filepath.Dir(link), 0755); err != nil {
+		return err
+	}
+	tmp := link + ".tmp"
+	os.Remove(tmp)
+	if err := os.Symlink(target, tmp); err != nil {
+		return err
+	}
+	return os.Rename(tmp, link)
 }
 
 func RemoveCurrent(base string) error {
